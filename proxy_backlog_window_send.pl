5e2746d0cc86af04eed9103e428131ebc4b6f38d
San GH <san.gh@clockwatching.net>
2011-11-28 22:36:09 -0800

    Make it use the nick when the script is loaded.

--- proxy_backlog_window_send.pl
+++ proxy_backlog_window_send.pl
@@ -25,7 +25,7 @@ $VERSION = "20111021";
 # Dir to store the autolog in.
 my $aldir = $ENV{"HOME"} . "/.irssi/autolog";
 # Channal that is used (created if needed) to write the blog to.
-my $bchan = "#backlog_san";
+my $bchan = "#backlog" . Irssi::Server->{ nick };
 # Milli pause between lines sent (servers may choke if the is too fast).
 my $waitline = 200;
 # If n lines is modulus this, pause for $waitclientbufferlines ms.
